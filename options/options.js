9cd8c5279d1523336979291a44ce5964e8d45940
Crowdbotics <team@crowdbotics.com>
2022-08-29 02:16:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "united_freedom_savi_36940",
+  url: "https://united_freedom_savi_36940.botics.co",
+  api: "https://united_freedom_savi_36940.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
